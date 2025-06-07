f74aeef2c15b774781fa2740192fcf6d302d23d6
Niasua <nsuarez2410@gmail.com>
2025-06-07 17:49:39 -0300

    Conditional statements added

--- MathGame_Niasua/Program.cs
+++ MathGame_Niasua/Program.cs
@@ -12,3 +12,31 @@ M - Multiplication
 D - Division
 Q - Quit the program");
 Console.WriteLine("------------------------------------------------------");
+
+string gameSelected = Console.ReadLine();
+
+if(gameSelected.Trim().ToLower() == "a")
+{
+    Console.WriteLine("Addition game selected");
+}
+else if (gameSelected.Trim().ToLower() == "s")
+{
+    Console.WriteLine("Substraction game selected");
+}
+else if (gameSelected.Trim().ToLower() == "m")
+{
+    Console.WriteLine("Multiplication game selected");
+}
+else if (gameSelected.Trim().ToLower() == "d")
+{
+    Console.WriteLine("Division game selected");
+}
+else if (gameSelected.Trim().ToLower() == "q")
+{
+    Console.WriteLine("Goodbye");
+    Environment.Exit(1);
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
