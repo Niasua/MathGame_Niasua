6df3bc965ffc3eac78e31705ec1b1f5b67ae6182
Niasua <nsuarez2410@gmail.com>
2025-06-07 20:55:47 -0300

    Most of the methods were refactored, putting them into classes

---
+++ MathGame_Niasua/GameEngine.cs
@@ -0,0 +1,151 @@
+﻿namespace MathGame_Niasua
+{
+    internal class GameEngine
+    {
+        internal void DivisionGame(string message)
+        {
+            var score = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+
+                int[] nums = Helpers.GetDivisionNumbers();
+                int firstNumber = nums[0];
+                int secondNumber = nums[1];
+
+                Console.WriteLine($"{firstNumber} / {secondNumber}");
+                var result = Console.ReadLine();
+
+                if (int.Parse(result) == firstNumber / secondNumber)
+                {
+                    Console.WriteLine("Your answer was correct! Type any key for the next question");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Your answer was incorrect! Type any key for the next question");
+                    Console.ReadLine();
+                }
+
+                if (i == 4)
+                {
+                    Console.WriteLine($"Game over. Your final score is {score}");
+                    Console.ReadLine();
+                }
+            }
+
+            Helpers.AddToHistory(score, "Division");
+        }
+        internal void MultiplicationGame(string message)
+        {
+            Random random = new Random();
+            var score = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                int firstNumber = random.Next(1, 9);
+                int secondNumber = random.Next(1, 9);
+
+                Console.WriteLine($"{firstNumber} * {secondNumber}");
+                var result = Console.ReadLine();
+
+                if (int.Parse(result) == firstNumber * secondNumber)
+                {
+                    Console.WriteLine("Your answer was correct! Type any key for the next question");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Your answer was incorrect! Type any key for the next question");
+                    Console.ReadLine();
+                }
+
+                if (i == 4)
+                {
+                    Console.WriteLine($"Game over. Your final score is {score}");
+                    Console.ReadLine();
+                }
+            }
+
+            Helpers.AddToHistory(score, "Multiplication");
+        }
+        internal void SubstractionGame(string message)
+        {
+            Random random = new Random();
+            var score = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                int firstNumber = random.Next(1, 9);
+                int secondNumber = random.Next(1, 9);
+
+                Console.WriteLine($"{firstNumber} - {secondNumber}");
+                var result = Console.ReadLine();
+
+                if (int.Parse(result) == firstNumber - secondNumber)
+                {
+                    Console.WriteLine("Your answer was correct! Type any key for the next question");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Your answer was incorrect! Type any key for the next question");
+                    Console.ReadLine();
+                }
+
+                if (i == 4)
+                {
+                    Console.WriteLine($"Game over. Your final score is {score}");
+                    Console.ReadLine();
+                }
+            }
+
+            Helpers.AddToHistory(score, "Substraction");
+        }
+        internal void AdditionGame(string message)
+        {
+            Random random = new Random();
+            var score = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Clear();
+                Console.WriteLine(message);
+                int firstNumber = random.Next(1, 9);
+                int secondNumber = random.Next(1, 9);
+
+                Console.WriteLine($"{firstNumber} + {secondNumber}");
+                var result = Console.ReadLine();
+
+                if (int.Parse(result) == firstNumber + secondNumber)
+                {
+                    Console.WriteLine("Your answer was correct! Type any key for the next question");
+                    score++;
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Your answer was incorrect! Type any key for the next question");
+                    Console.ReadLine();
+                }
+
+                if (i == 4)
+                {
+                    Console.WriteLine($"Game over. Your final score is {score}");
+                    Console.ReadLine();
+                }
+            }
+
+            Helpers.AddToHistory(score, "Addition");
+        }
+    }
+}
---
+++ MathGame_Niasua/Helpers.cs
@@ -0,0 +1,51 @@
+﻿namespace MathGame_Niasua
+{
+    internal class Helpers
+    {
+        static List<string> games = new();
+        internal static string GetName()
+        {
+            Console.Write("Please type your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        internal static int[] GetDivisionNumbers()
+        {
+            Random Random = new Random();
+            int firstNumber = Random.Next(0, 99);
+            int secondNumber = Random.Next(0, 99);
+
+            int[] result = new int[2];
+
+            // the division must result in an integer
+            // the second number must be != 0 and greater than the first number
+            while (firstNumber % secondNumber != 0 && secondNumber == 0 && secondNumber > firstNumber)
+            {
+                firstNumber = Random.Next(0, 99);
+                secondNumber = Random.Next(0, 99);
+            }
+
+            result[0] = firstNumber;
+            result[1] = secondNumber;
+
+            return result;
+        }
+        internal static void GetGames()
+        {
+            Console.Clear();
+            Console.WriteLine("Games History");
+            Console.WriteLine("------------------------------------------------------");
+            foreach (var game in games)
+            {
+                Console.WriteLine(game);
+            }
+            Console.WriteLine("------------------------------------------------------\n");
+            Console.WriteLine("Press any key to go back to the menu");
+            Console.ReadLine();
+        }
+        internal static void AddToHistory(int score, string game)
+        {
+            games.Add($"{DateTime.Now} - {game}: Score {score}");
+        }
+    }
+}
