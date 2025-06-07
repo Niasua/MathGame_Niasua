d7b07816bca9da202d89f21af28fd55eea4ac248
Niasua <nsuarez2410@gmail.com>
2025-06-07 16:07:38 -0300

    Initial menu template

--- MathGame_Niasua/Program.cs
+++ MathGame_Niasua/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Please type your name: ");
+string name = Console.ReadLine();
+DateTime date = DateTime.UtcNow;
+
+Console.WriteLine("------------------------------------------------------");
+Console.WriteLine($"Hello {name}. It's {date}. This is your math's game. ");
+Console.WriteLine("\n");
+Console.WriteLine(@$"What game would you like to play? Choose from the options below: 
+A - Addition
+S - Substraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+Console.WriteLine("------------------------------------------------------");
