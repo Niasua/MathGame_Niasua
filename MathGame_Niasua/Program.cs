06913f2fd53af30c92b1a254badaa047baf34024
Niasua <nsuarez2410@gmail.com>
2025-06-07 18:07:03 -0300

    the menu is now a method

--- MathGame_Niasua/Program.cs
+++ MathGame_Niasua/Program.cs
@@ -2,44 +2,47 @@
 string name = Console.ReadLine();
 DateTime date = DateTime.UtcNow;
 
-Console.WriteLine("------------------------------------------------------");
-Console.WriteLine($"Hello {name}. It's {date}. This is your math's game. ");
-Console.WriteLine("\n");
-Console.WriteLine(@$"What game would you like to play? Choose from the options below: 
-A - Addition
-S - Substraction
-M - Multiplication
-D - Division
-Q - Quit the program");
-Console.WriteLine("------------------------------------------------------");
+Menu(name, date);
 
-string gameSelected = Console.ReadLine();
-
-switch(gameSelected.Trim().ToLower())
+void Menu(string name, DateTime date)
 {
-    case "a":
-        AdditionGame("Addition game selected");
-        break;
-    case "s":
-        SubstractionGame("Substraction game selected");
-        break;
-    case "m":
-        MultiplicationGame("Multiplication game selected");
-        break;
-    case "d":
-        DivisionGame("Division game selected");
-        break;
-    case "q":
-        Console.WriteLine("Goodbye");
-        Environment.Exit(1);
-        break;
-    default:
-        Console.WriteLine("Invalid input");
-        Environment.Exit(1);
-        break;
+    Console.WriteLine("------------------------------------------------------");
+    Console.WriteLine($"Hello {name}. It's {date}. This is your math's game. ");
+    Console.WriteLine("\n");
+    Console.WriteLine(@$"What game would you like to play? Choose from the options below: 
+    A - Addition
+    S - Substraction
+    M - Multiplication
+    D - Division
+    Q - Quit the program");
+    Console.WriteLine("------------------------------------------------------");
+
+    string gameSelected = Console.ReadLine();
+
+    switch (gameSelected.Trim().ToLower())
+    {
+        case "a":
+            AdditionGame("Addition game selected");
+            break;
+        case "s":
+            SubstractionGame("Substraction game selected");
+            break;
+        case "m":
+            MultiplicationGame("Multiplication game selected");
+            break;
+        case "d":
+            DivisionGame("Division game selected");
+            break;
+        case "q":
+            Console.WriteLine("Goodbye");
+            Environment.Exit(1);
+            break;
+        default:
+            Console.WriteLine("Invalid input");
+            Environment.Exit(1);
+            break;
+    }
 }
-
-
 void DivisionGame(string message)
 {
     Console.WriteLine(message);
